264e568b68518129d7cd77c67a589527191a8e2e
Boris Karavasilev <xkarav01@vutbr.cz>
2021-04-10 15:35:56 +0200

    Update GenerateNodesNoises.cs

--- Assets/Scripts/IslandAreaTest/Tasks/Generate Nodes Noises/GenerateNodesNoises.cs
+++ Assets/Scripts/IslandAreaTest/Tasks/Generate Nodes Noises/GenerateNodesNoises.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenerateNodesNoises : MonoBehaviour
+public class GenerateNodesNoises : SingleTask
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Inputs
+	private int resolution;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Inputs from previous task
+	private Func<List<TerrainNode>> getTerrainNodes;
+	private List<TerrainNode> terrainNodes;
+
+	// Outputs
+	private List<Color[]> noises;
+
+	// Internal
+	private int texturePixelCount;
+	private PerlinNoise2D perlinNoise;
+
+	public GenerateNodesNoises(int resolution, Noise2DParams noiseParams, Func<List<TerrainNode>> getTerrainNodes, int stepSize = 1)
+	{
+		Name = "Generate Nodes Noises";
+		StepSize = stepSize;
+
+		this.resolution = resolution;
+		this.getTerrainNodes = getTerrainNodes;
+
+		noises = new List<Color[]>();
+		texturePixelCount = resolution * resolution;
+	}
+
+	protected override void ExecuteStep()
+	{
+		int nodeIndex = ExecutedSteps;
+		Color[] noise = new Color[texturePixelCount];
+
+		for (int pixelIndex = 0; pixelIndex < texturePixelCount; pixelIndex++)
+		{
+			Vector2Int pixel2DCoords = TextureFunctions.ArrayIndexToCoords(resolution, resolution, pixelIndex);
+			float intensity = perlinNoise.GetValue(pixel2DCoords);
+			Color pixelColor = new Color(intensity, intensity, intensity);
+			noise[pixelIndex] = pixelColor;
+		}
+	}
+
+	protected override void GetInputFromPreviousStep()
+	{
+		terrainNodes = getTerrainNodes();
+	}
+
+	protected override void SetSteps()
+	{
+		TotalSteps = terrainNodes.Count;
+		RemainingSteps = TotalSteps;
+	}
 }
