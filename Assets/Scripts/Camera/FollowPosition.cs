bc1c1688b90f33848fade839267429968d1556bd
Boris Karavasilev <xkarav01@vutbr.cz>
2021-05-02 15:57:23 +0200

    Added zooming with keyboard.

--- Assets/Scripts/Camera/FollowPosition.cs
+++ Assets/Scripts/Camera/FollowPosition.cs
@@ -3,7 +3,11 @@
 [RequireComponent(typeof(Camera))]
 public class FollowPosition : MonoBehaviour
 {
-	public Transform target;
+	[SerializeField] private Transform target;
+	[SerializeField] private float cameraZoomSpeed = 1f;
+	[SerializeField] private float minFOV = 15f;
+	[SerializeField] private float maxFOV = 70f;
+
 	private Vector3 offset;
 	private Camera followingCamera;
 
@@ -18,10 +22,27 @@ public class FollowPosition : MonoBehaviour
     void Update()
     {
 	    transform.position = target.position + offset;
-    }
+	    followingCamera.fieldOfView -= Input.mouseScrollDelta.y * cameraZoomSpeed;
+	}
 
-    void OnGUI()
+    void FixedUpdate()
     {
-	    followingCamera.fieldOfView -= Input.mouseScrollDelta.y;
-    }
+	    HandleZoom();
+	}
+
+    private void HandleZoom()
+    {
+	    bool shiftPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+		bool ctrlPressed = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+
+		if (shiftPressed && followingCamera.fieldOfView > minFOV)
+	    {
+		    followingCamera.fieldOfView -= cameraZoomSpeed;
+		}
+
+		if (ctrlPressed && followingCamera.fieldOfView < maxFOV)
+		{
+			followingCamera.fieldOfView += cameraZoomSpeed;
+		}
+	}
 }
